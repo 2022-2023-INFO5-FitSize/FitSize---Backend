3d0dc567f803a3c9bfcdf664ebf2b124267ca45d
AltJackBox <ya38able@gmail.com>
2022-03-07 15:41:50 +0100

    Initial commit

---
+++ polls/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    login = models.CharField(max_length=100, blank=True)
+    password = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return self.login
+
+class UserModel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, blank=True)
+    dimensions = models.CharField(max_length=100, blank=True)
+    user = models.ForeignKey( 'User', on_delete=models.CASCADE, blank=True, null=True)
+    clothingtype = models.ForeignKey( 'ClothingType', on_delete=models.CASCADE, blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+class Company(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, blank=True)
+    adress = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return self.name
+
+class CompanyModel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    color = models.CharField(max_length=100, blank=True)
+    dimensions = models.CharField(max_length=100, blank=True)
+    company = models.ForeignKey( 'Company', on_delete=models.CASCADE, blank=True, null=True)
+    size = models.ForeignKey( 'Size', on_delete=models.CASCADE, blank=True, null=True)
+    clothingtype = models.ForeignKey( 'ClothingType', on_delete=models.CASCADE, blank=True, null=True)
+    def __str__(self):
+        return self.id
+
+class Size(models.Model):
+    id = models.IntegerField(primary_key=True)
+    label = models.CharField(max_length=100, blank=True)
+    origin = models.CharField(max_length=100, blank=True)
+    def __str__(self):
+        return self.label
+
+class ClothingType(models.Model):
+    id = models.IntegerField(primary_key=True)
+    label = models.CharField(max_length=100, blank=True)
+    points = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return self.label
