68ffee4299d2376ee08584bff14983af4b7e6b23
AltJackBox <ya38able@gmail.com>
2022-03-09 16:51:53 +0100

    Completion of Factory Boy models
    Ended first unitary test, completed setup to conduct tests using our models

--- tests/models.py
+++ tests/models.py
@@ -1,81 +1,55 @@
 import factory
 import uuid
-from django.test import TestCase
-from polls.models import User, UserModel
+from polls.models import ClothingType, Company, User, UserModel, CompanyModel, Size
 from django.db import models
 
 class UserFactory(factory.Factory):
-    id = models.UUIDField(
-         primary_key = True,
-         default = uuid.uuid4,
-         editable = False)
-    login = factory.Faker('login')
+    id =  uuid.uuid4()
+    login = factory.Faker('name')
     password = factory.Faker('password')
     class Meta:
         model = User
 
-# class UserTest(TestCase):
-#     id = 1
-#     login = "Adam"
-#     password = "password"
-#     def __str__(self):
-#         return self.login
+class ClothingTypeFactory(factory.Factory):
+    id = uuid.uuid4()
+    label = "XS"
+    points = models.CharField(max_length=200, blank=True)
+    class Meta:
+        model = ClothingType
 
-class UserModelFactory(UserModel):
-    id = models.UUIDField(primary_key = True,
-         default = uuid.uuid4,
-         editable = False)
+class UserModelFactory(factory.Factory):
+    id = uuid.uuid4()
     name = factory.Faker('name')
-    dimensions = "200 10 100" #Syntaxe des dimensions à définir
-    # user = User(id = 1, login = "John", password = "One")
-    # clothingtype = ClothingType(id = 1, label = "Robe", points = "1 0 0 0.1")
+    dimensions = "200 10 100"
+    user = factory.SubFactory(UserFactory)
+    clothingtype = factory.SubFactory(ClothingTypeFactory)
     class Meta:
         model = UserModel
-    
-    @factory.post_generation
-    def user(obj, create, extracted, **kwargs):
-        if not create:
-            return
-        if extracted:
-            assert isinstance(extracted, int)
-            UserFactory.create_batch(size=extracted, **kwargs)
 
-# class UserModelTest(TestCase):
-#     id = 1
-#     name = "TShirt"
-#     dimensions = "200 10 100" #Syntaxe des dimensions à définir
-#     user = User(id = 1, login = "John", password = "One")
-#     clothingtype = ClothingType(id = 1, label = "Robe", points = "1 0 0 0.1")
-#     def __str__(self):
-#         return self.name + " " + self.user.login
+class CompanyFactory(factory.Factory):
+    id = uuid.uuid4()
+    name = factory.Faker('company')
+    adress = factory.Faker('address')
+    class Meta:
+        model = Company
 
-# class Company(TestCase):
-#     id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=100, blank=True)
-#     adress = models.CharField(max_length=200, blank=True)
-#     def __str__(self):
-#         return self.name
 
-# class CompanyModel(TestCase):
-#     id = models.IntegerField(primary_key=True)
-#     color = models.CharField(max_length=100, blank=True)
-#     dimensions = models.CharField(max_length=100, blank=True)
-#     company = models.ForeignKey( 'Company', on_delete=models.CASCADE, blank=True, null=True)
-#     size = models.ForeignKey( 'Size', on_delete=models.CASCADE, blank=True, null=True)
-#     clothingtype = models.ForeignKey( 'ClothingType', on_delete=models.CASCADE, blank=True, null=True)
-#     def __str__(self):
-#         return self.id
+class SizeFactory(factory.Factory):
+    id = uuid.uuid4()
+    label = "XS"
+    origin = factory.Faker('country')
+    class Meta:
+        model = Size
 
-# class Size(TestCase):
-#     id = models.IntegerField(primary_key=True)
-#     label = models.CharField(max_length=100, blank=True)
-#     origin = models.CharField(max_length=100, blank=True)
-#     def __str__(self):
-#         return self.label
+class CompanyModelFactory(factory.Factory):
+    id = uuid.uuid4()
+    color = factory.Faker('color')
+    dimensions = "200 10 100"
+    company = factory.SubFactory(CompanyFactory)
+    size = factory.SubFactory(SizeFactory)
+    clothingtype = factory.SubFactory(ClothingTypeFactory)
+    class Meta:
+        model = CompanyModel
 
-# class ClothingType(TestCase):
-#     id = models.IntegerField(primary_key=True)
-#     label = models.CharField(max_length=100, blank=True)
-#     points = models.CharField(max_length=200, blank=True)
-#     def __str__(self):
-#         return self.label
+
+
