fd938f5a1b4657e68167f43a86eac9ef61c1b934
Jules GONZALEZ <julesgonzalez99@yahoo.fr>
2022-03-09 14:28:20 +0100

    feat: views and url

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
+from django.conf.urls import include, re_path
+from rest_framework.routers import DefaultRouter
+
+from polls.models import ClothingType, Company, CompanyModel, Size, User, UserModel
+from .views import ClothingTypeViewSet, CompanyModelViewSet, CompanyViewSet, SizeViewSet, UserModelViewSet, UserViewSet
 
 from . import views
 
+router = DefaultRouter()
+router.register(User, UserViewSet, base_name='user')
+router.register(UserModel, UserModelViewSet, base_name='user-model')
+router.register(Company, CompanyViewSet, base_name='company')
+router.register(CompanyModel, CompanyModelViewSet, base_name='company-model')
+router.register(Size, SizeViewSet, base_name='size')
+router.register(ClothingType, ClothingTypeViewSet, base_name='clothing-type')
+
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path('', views.index, name='index'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,64 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework.mixins import (
+    CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+)
+from rest_framework.viewsets import GenericViewSet
+
+from .models import ClothingType, Company, CompanyModel, Size, User, UserModel
+from .serializers import ClothingTypeSerializer, CompanyModelSerializer, CompanySerializer, SizeSerializer, UserModelSerializer, UserSerializer
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-# Create your views here.
+class UserViewSet(GenericViewSet,  # generic view functionality
+                     CreateModelMixin,  # handles POSTs
+                     RetrieveModelMixin,  # handles GETs for 1 User
+                     UpdateModelMixin,  # handles PUTs and PATCHes
+                     ListModelMixin):  # handles GETs for many Users
+      serializer_class = UserSerializer
+      queryset = User.objects.all()
+
+class UserModelViewSet(GenericViewSet,
+                     CreateModelMixin,
+                     RetrieveModelMixin,
+                     UpdateModelMixin,
+                     ListModelMixin):
+      serializer_class = UserModelSerializer
+      queryset = UserModel.objects.all()
+
+class CompanyViewSet(GenericViewSet,
+                     CreateModelMixin,
+                     RetrieveModelMixin,
+                     UpdateModelMixin,
+                     ListModelMixin):
+      serializer_class = CompanySerializer
+      queryset = Company.objects.all()
+
+class CompanyModelViewSet(GenericViewSet,
+                     CreateModelMixin,
+                     RetrieveModelMixin,
+                     UpdateModelMixin,
+                     ListModelMixin):
+      serializer_class = CompanyModelSerializer
+      queryset = CompanyModel.objects.all()
+
+
+class SizeViewSet(GenericViewSet,
+                     CreateModelMixin,
+                     RetrieveModelMixin,
+                     UpdateModelMixin,
+                     ListModelMixin):
+      serializer_class = SizeSerializer
+      queryset = Size.objects.all()
+
+
+class ClothingTypeViewSet(GenericViewSet,
+                     CreateModelMixin,
+                     RetrieveModelMixin,
+                     UpdateModelMixin,
+                     ListModelMixin):
+      serializer_class = ClothingType
+      queryset = ClothingTypeSerializer.objects.all()
