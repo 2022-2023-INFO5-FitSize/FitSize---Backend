bf6116295d5ad61c7bdffaefffdeba5bba7333cb
AltJackBox <ya38able@gmail.com>
2022-03-16 11:20:17 +0100

    Implemented new endpoint
    New endpoint for UserModels : usermodel/savedimensions/
    Converts strings 'dimensions' according to the points recieved and the scale transmitted
    Improvements needs to be done to accept any series of coordinates & to prevent errors with syntax of the request

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 from asyncio.windows_events import NULL
 from math import sqrt
 from rest_framework.decorators import action
-from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.viewsets import ModelViewSet
 from polls.models import ClothingType, Company, CompanyModel, Size, User, UserModel
@@ -19,21 +18,31 @@ class UserModelViewSet(ModelViewSet):
       serializer_class = UserModelSerializer
       queryset = UserModel.objects.all()
 
-      @action(methods=['post'])
+      @action(methods=['post'], detail=False, url_path='savedimensions',url_name="Save dimensions")
       def saveFromKeyPoints(self, request, *args, **kwargs) :
-            userModels = UserModel.objects.all()
-            data = request.data
-            keypoints = data.keypoints
-            controlobject = data.control.object
-            scale = controlobject.length / sqrt((controlobject.point1.x - controlobject.point2.x)**2 + (controlobject.point1.y - controlobject.point2.y)**2)
-            dim1 = scale * sqrt((keypoints.point1.x - keypoints.point2.x)**2 + (keypoints.point1.y - keypoints.point2.y)**2)
-            dim2 = scale * sqrt((keypoints.point3.x - keypoints.point4.x)**2 + (keypoints.point3.y - keypoints.point4.y)**2)
-            dim3 = scale * sqrt((keypoints.point5.x - keypoints.point6.x)**2 + (keypoints.point5.y - keypoints.point6.y)**2)
-            serializer = UserModelSerializer(userModels, name=data.name, dimensions=[dim1,dim2,dim3])
-            if serializer.is_valid :
-                  serializer.save()
-                  return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            array = request.data['dimensions']
+            array = array.split('KP')
+            if len(array) != 2:
+                  return Response({"Failure": "Wrong syntax for dimensions field"}, status=status.HTTP_400_BAD_REQUEST)
+            keypoints = array[1].split(',')
+            controlobject = array[0].split(',')
+
+            arr_control = [float(i) for i in controlobject]
+            arr_keypoints = [float(j) for j in keypoints]
+
+            # scale = controlobject.length / sqrt((controlobject.point1.x - controlobject.point2.x)**2 + (controlobject.point1.y - controlobject.point2.y)**2)
+            # dim1 = scale * sqrt((keypoints.point1.x - keypoints.point2.x)**2 + (keypoints.point1.y - keypoints.point2.y)**2)
+            # dim2 = scale * sqrt((keypoints.point3.x - keypoints.point4.x)**2 + (keypoints.point3.y - keypoints.point4.y)**2)
+            # dim3 = scale * sqrt((keypoints.point5.x - keypoints.point6.x)**2 + (keypoints.point5.y - keypoints.point6.y)**2)
+
+            scale = float(len(arr_control)) / sqrt((arr_control[0] - arr_control[2])**2 + (arr_control[1] - arr_control[3])**2)
+            dim1 = scale * sqrt((arr_keypoints[0] - arr_keypoints[2])**2 + (arr_keypoints[1] - arr_keypoints[3])**2)
+            dim2 = scale * sqrt((arr_keypoints[4] - arr_keypoints[6])**2 + (arr_keypoints[5] - arr_keypoints[7])**2)
+            dim3 = scale * sqrt((arr_keypoints[8] - arr_keypoints[10])**2 + (arr_keypoints[9] - arr_keypoints[11])**2)
+
+            request.data['dimensions'] = str(dim1) + "," + str(dim2) + "," + str(dim3)
+            #return Response(request.data, status=status.HTTP_201_CREATED)
+            return self.create(request, *args, **kwargs)
 
 class CompanyViewSet(ModelViewSet):
       serializer_class = CompanySerializer
