2cef2428732e9805afc6be411bb659c269c084fc
Florian Chappaz <Florian.Chappaz@etu.univ-grenoble-alpes.fr>
2023-02-07 16:13:50 +0100

    API endpoint for executing detection script with clothing type and image in body

--- keypoints/views.py
+++ keypoints/views.py
@@ -1,35 +1,48 @@
+import base64
 import json
 import os
 import re
 import subprocess
 
 from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
 
 
+# @csrf_exempt
 def exec_script(request):
-    # TODO: take base64 in the request, save tmp img and give its path to the script
-    wd = os.getcwd() + '/keypoints/code'
-    ai_exec = subprocess.Popen(['python', 'run_no_det.py',
-                                '--clothing', 'trousers'],
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT,
-                               cwd=wd)
-    res = ai_exec.communicate()[0]
-    print(res)
-    # os.remove(os.getcwd() + '/keypoints/code/tmp.jpg')
+    res = {}
+    if request.method == 'POST':
+        body_json = json.loads(request.body)
+        clothing = body_json['clothing']
+        img_data = body_json['image'] # base64 raw data (without prefix data:image/...)
+        wd = os.getcwd() + '/keypoints/code'
 
-    # Extract keypoints and checkerboard size
-    split_res = res.split(b'\n')
-    keypoints = json.loads(
-        re.sub(
-            r"array\([^)]*\)",
-            lambda s: '"' + s.group(0) + '"',
-            split_res[3].decode("utf-8").replace("'", '"')
+        with open(wd + '/tmp.jpg', 'wb') as f:
+            f.write(base64.b64decode(img_data)) # Valid only for jpg data
+            f.close()
+
+        ai_exec = subprocess.Popen(['python', 'run_no_det.py',
+                                    '--clothing', clothing,
+                                    '--source', wd + '/tmp.jpg'],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT,
+                                   cwd=wd)
+        res = ai_exec.communicate()[0]
+        os.remove(os.getcwd() + '/keypoints/code/tmp.jpg')
+
+        # Extract keypoints and checkerboard size
+        split_res = res.split(b'\n')
+        keypoints = json.loads(
+            re.sub(
+                r"array\([^)]*\)",
+                lambda s: '"' + s.group(0) + '"',
+                split_res[3].decode("utf-8").replace("'", '"')
+            )
         )
-    )
-    cb_box_distance = float(split_res[4].decode("utf-8"))
+        cb_box_distance = float(split_res[4].decode("utf-8"))
+        res = {
+            'keypoints': keypoints,
+            'cb_box_distance': cb_box_distance,
+        }
 
-    return JsonResponse({
-        'keypoints': keypoints,
-        'cb_box_distance': cb_box_distance,
-    })
+    return JsonResponse(res)
